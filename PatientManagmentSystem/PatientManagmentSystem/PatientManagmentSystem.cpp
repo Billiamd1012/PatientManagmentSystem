e804e83a7f7a39d3e68c10c247f91465c29e4375
William Darker <62284382+Billiamd1012@users.noreply.github.com>
2025-05-22 15:48:12 +0930

    Initial commit

---
+++ PatientManagmentSystem/PatientManagmentSystem/PatientManagmentSystem.cpp
@@ -0,0 +1,12 @@
+﻿// PatientManagmentSystem.cpp : Defines the entry point for the application.
+//
+
+#include "PatientManagmentSystem.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
